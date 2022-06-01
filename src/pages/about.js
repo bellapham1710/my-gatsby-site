d0e3f7fd3293daebfe6e0015ffe01bdc91af4b7d
Bella Pham <unpham@email.sc.edu>
2022-05-31 21:47:03 -0400

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+    return (
+        <main>
+            <title>Bella Pham</title>
+            <h1>ABout Me</h1>
+            <p>Hi! My name is Bella Pham. I’m twenty-one years old. I’m an undergraduate junior major in Information Science from the University of South Carolina.</p>
+
+        </main>
+    )
+
+
+}
+// Step 3: Export your component
+export default AboutPage
