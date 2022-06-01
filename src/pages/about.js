60c1e2553bc9bb4e5778ea84fcf094f6672b5521
Bella Pham <unpham@email.sc.edu>
2022-05-31 22:12:26 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,12 +9,10 @@ const AboutPage = () => {
             <title>Bella Pham</title>
             <h1>About Me</h1>
             <p>Hi! My name is Bella Pham. I’m twenty-one years old. I’m an undergraduate junior major in Information Science from the University of South Carolina.</p>
-            <Link to ="/">Home</Link>
+            <Link to ="/">Back to Home</Link>
 
         </main>
     )
-
-
 }
 // Step 3: Export your component
 export default AboutPage
