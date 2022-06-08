c880bdde10b68b9c9ff26a322491856c27ed44f9
Bella Pham <unpham@email.sc.edu>
2022-06-08 01:29:29 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,18 @@
 // Step 1: Import React
 import * as React from 'react'
+import Layout from "../components/layout"
 import {Link} from 'gatsby'
 
 // Step 2: Define your component
 const AboutPage = () => {
     return (
         <main>
+            <Layout>
             <title>Bella Pham</title>
             <h1>About Me</h1>
             <p>Hi! My name is Bella Pham. I’m twenty-one years old. I’m an undergraduate junior major in Information Science from the University of South Carolina.</p>
             <Link to ="/">Back to Home</Link>
-
+            </Layout>
         </main>
     )
 }
