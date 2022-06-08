5c3965e30edd105d44c7bf9d4f5d75271cbc2727
Bella Pham <unpham@email.sc.edu>
2022-06-07 22:07:18 -0400

    Summary

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,29 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
---
+++ src/pages/blog.js
@@ -0,0 +1,18 @@
+// Step 1: Import React
+import * as React from 'react'
+import {Link} from 'gatsby'
+
+// Step 2: Define your component
+const AboutPage = () => {
+    return (
+        <main>
+            <title>Bella Pham</title>
+            <h1>About Me</h1>
+            <p>Hi! My name is Bella Pham. I’m twenty-one years old. I’m an undergraduate junior major in Information Science from the University of South Carolina.</p>
+            <Link to ="/">Back to Home</Link>
+
+        </main>
+    )
+}
+// Step 3: Export your component
+export default BlogPage
